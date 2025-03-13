77617f5ba9122e76aa43926b815ccfd60311d233
lining <lo@qq.com>
2025-03-13 15:38:03 +0800

    拆分Home首页的剩余组件

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,15 +1,35 @@
 <template>
   <div>
-    <TypeNav/>
+    <TypeNav />
+    <ListContainer />
+    <Recommend />
+    <Rank />
+    <Like />
+    <Floor />
+    <Floor />
+    <Brand />
   </div>
 </template>
 
 <script>
-    export default {
-        name:'Home'
-    }
+import ListContainer from './ListContainer'
+import Recommend from './Recommend'
+import Rank from './Rank'
+import Like from './Like'
+import Floor from './Floor'
+import Brand from './Brand'
+
+export default {
+  name: 'Home',
+  components:{
+    ListContainer,
+    Recommend,
+    Rank,
+    Like,
+    Floor,
+    Brand
+  }
+}
 </script>
 
-<style lang='less' scoped>
-
-</style>
+<style lang='less' scoped></style>
